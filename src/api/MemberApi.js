d87fd257b01dd93f7be40e3dd117abbb78f078f9
tjoeun <gaemkyu4@gmail.com>
2024-06-18 16:15:31 +0900

    test

--- src/api/MemberApi.js
+++ src/api/MemberApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import jwtAxios from "./../util/jwtUtil";
 
-export const API_SERVER_HOST = "http://localhost:8080";
+export const API_SERVER_HOST = "http://192.168.100.36:8080";
 const prefix = `${API_SERVER_HOST}/api/member`;
 
 // 회원가입 요청
--- src/pages/chat/ChatRoomPage.js
+++ src/pages/chat/ChatRoomPage.js
@@ -23,7 +23,7 @@ const ChatRoomPage = () => {
       return;
     }
 
-    const socket = new SockJS("http://localhost:8080/chat");
+    const socket = new SockJS("http://192.168.100.36:8080/chat");
     const stompClient = Stomp.over(socket);
 
     stompClient.connect(
